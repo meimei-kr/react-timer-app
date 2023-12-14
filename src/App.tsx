9530d8c87b03f4f138a7da67018b7b3acfd3028f
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2023-12-14 22:13:04 +0900

    Update: useReducerに書き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useReducer, useEffect } from 'react'
 import styled from '@emotion/styled'
 import useSound from 'use-sound'
 import beepSound from './sounds/beep.mp3'
@@ -28,21 +28,59 @@ const SErrorMessage = styled.div`
   text-align: center;
 `
 
+const initialState = {
+  inputMinutes: 0,
+  inputSeconds: 0,
+  totalSeconds: 0,
+  isRunning: false,
+  errors: []
+}
+
+type State = {
+  inputMinutes: number
+  inputSeconds: number
+  totalSeconds: number
+  isRunning: boolean
+  errors: string[]
+}
+
+type Action = 
+  | { type: "SET_MINUTES"; payload: number  }
+  | { type: "SET_SECONDS"; payload: number  }
+  | { type: "SET_TOTAL_SECONDS"; payload: number  }
+  | { type: "SET_IS_RUNNING"; payload: boolean  }
+  | { type: "SET_ERRORS"; payload: string[] }
+  | { type: "RESET" }
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "SET_MINUTES":
+      return { ...state, inputMinutes: action.payload }
+    case "SET_SECONDS":
+      return { ...state, inputSeconds: action.payload }
+    case "SET_TOTAL_SECONDS":
+      return { ...state, totalSeconds: action.payload }
+    case "SET_IS_RUNNING":
+      return { ...state, isRunning: action.payload }
+    case "SET_ERRORS":
+      return { ...state, errors: action.payload }
+    case "RESET":
+      return { ...initialState }
+    default:
+      return state
+  }
+}
+
 const App = () => {
-  const [inputMinutes, setInputMinutes] = useState<number>(0)
-  const [inputSeconds, setInputSeconds] = useState<number>(0)
-  const [totalSeconds, setTotalSeconds] = useState<number>(0)
-  const [isRunning, setIsRunning] = useState(false)
-  const [errors, setErrors] = useState<string[]>([])
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { inputMinutes, inputSeconds, totalSeconds, isRunning, errors } = state
 
   const [play] = useSound(beepSound, { volume: 0.5 })
 
   const minutes = Math.floor(totalSeconds / 60)
   const seconds = totalSeconds % 60
-  let initialTotalSeconds = 0
 
   const handleStart = () => {
-
     let newErrors: string[] = []
     if (inputMinutes < 0 || inputMinutes > 59 ) {
       newErrors.push('分は0〜59の数値を入力してください')
@@ -51,37 +89,33 @@ const App = () => {
       newErrors.push('秒は0〜59の数値を入力してください')
     }
     if (newErrors.length > 0) {
-      setErrors(newErrors)
+      dispatch({ type: "SET_ERRORS", payload: newErrors })
       return
     }
-    setErrors([])
-    setTotalSeconds(inputMinutes * 60 + inputSeconds)
-    initialTotalSeconds = totalSeconds
-    setIsRunning(true)
+    dispatch({ type: "SET_ERRORS", payload: [] })
+    dispatch({ type: 'SET_TOTAL_SECONDS', payload: inputMinutes * 60 + inputSeconds });
+    dispatch({ type: 'SET_IS_RUNNING', payload: true });
   }
 
   const handleStop = () => {
-    setIsRunning(false)
+    dispatch({ type: "SET_IS_RUNNING", payload: false })
   }
 
   const handleReset = () => {
-    setTotalSeconds(0)
-    setIsRunning(false)
-    setInputMinutes(0)
-    setInputSeconds(0)
+    dispatch({ type: "RESET" })
   }
 
   const handleMinutesChange = (minutes: number) => {
-    setInputMinutes(minutes)
+    dispatch({ type: "SET_MINUTES", payload: minutes })
     if (!isRunning) {
-      setTotalSeconds(minutes * 60 + inputSeconds)
+      dispatch({ type: "SET_TOTAL_SECONDS", payload: minutes * 60 + inputSeconds })
     }
   }
 
   const handleSecondsChange = (seconds: number) => {
-    setInputSeconds(seconds)
+    dispatch({ type: "SET_SECONDS", payload: seconds })
     if (!isRunning) {
-      setTotalSeconds(inputMinutes * 60 + seconds)
+      dispatch({ type: "SET_TOTAL_SECONDS", payload: inputMinutes * 60 + seconds })
     }
   }
 
@@ -90,15 +124,12 @@ const App = () => {
 
     if (isRunning && totalSeconds > 0) {
       interval = setInterval(() => {
-        setTotalSeconds(prevTotalSeconds => prevTotalSeconds - 1)
+        dispatch({ type: "SET_TOTAL_SECONDS", payload: totalSeconds - 1 })
       }, 1000)
     } else if (isRunning && totalSeconds === 0) {
       play()
       clearInterval(interval)
-      setTotalSeconds(0)
-      setIsRunning(false)
-      setInputMinutes(0)
-      setInputSeconds(0)
+      dispatch({ type: "RESET" })
     }
 
     return () => {
@@ -122,7 +153,7 @@ const App = () => {
         <Button handleClick={handleStop}>一時停止</Button>
         <Button handleClick={handleReset}>リセット</Button>
       </SButtons>
-      {errors && errors.map(error => <SErrorMessage>{error}</SErrorMessage>)}
+      {errors.length > 0 && errors.map((error, index) => <SErrorMessage key={index}>{error}</SErrorMessage>)}
     </div>
   )
 }
